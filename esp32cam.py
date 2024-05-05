5084bdec930fe898acfabbd38c1836b6c8ae1ece
ppastabtw <130695819+ppastabtw@users.noreply.github.com>
2024-05-04 21:38:16 -0400

    ESP Cam Code
    - Complete code to access images through esp cam.

---
+++ esp32cam.py
@@ -0,0 +1,94 @@
+import cv2
+import numpy as np
+import urllib.request
+import http.client
+
+#check ip addresses
+url2 = 'http://192.168.1.112/cam-lo.jpg'
+url1 = 'http://192.168.1.128/cam-lo.jpg'
+
+cap1 = cv2.VideoCapture(url1)
+cap2 = cv2.VideoCapture(url2)
+whT=320
+confThreshold = 0.5
+nmsThreshold = 0.3
+classesfile='coco.names'
+classNames=[]
+with open(classesfile,'rt') as f:
+    classNames=f.read().rstrip('\n').split('\n')
+
+
+modelConfig = 'yolov3.cfg'
+modelWeights= 'yolov3.weights'
+net = cv2.dnn.readNetFromDarknet(modelConfig,modelWeights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+def findObject(outputs,img):
+    hT,wT,cT = img.shape
+    bbox = []
+    classIds = []
+    confs = []
+    for output in outputs:
+        for det in output:
+            scores = det[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > confThreshold:
+                if wT == 0 or hT == 0 or len(classIds) > 30:
+                    continue
+                w,h = int(det[2]*wT), int(det[3]*hT)
+                x,y = int((det[0]*wT)-w/2), int((det[1]*hT)-h/2)
+                bbox.append([x,y,w,h])
+                classIds.append(classId)
+                confs.append(float(confidence))
+    #print(len(bbox))q
+    indices = cv2.dnn.NMSBoxes(bbox,confs,confThreshold,nmsThreshold)
+
+    if(len(classIds) < 30):
+        for i in indices:
+            i = i
+            box = bbox[i]
+            x,y,w,h = box[0],box[1],box[2],box[3]
+            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,255),2)
+            cv2.putText(img, f'{classNames[classIds[i]].upper()} {int(confs[i]*100)}%', (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,0,255), 2)
+ 
+       
+
+
+
+import threading
+
+def fetch_frame(url, cap, net, window_name):
+    while True:
+        try:
+            img_resp = urllib.request.urlopen(url)
+            imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
+            im = cv2.imdecode(imgnp, -1)
+            # success, img = cap.read()
+            blob = cv2.dnn.blobFromImage(im, 1/255, (whT, whT), [0,0,0], 1, crop=False)
+            net.setInput(blob)
+            layer_names = net.getLayerNames()
+            output_names = [layer_names[i-1] for i in net.getUnconnectedOutLayers()]
+            outputs = net.forward(output_names)
+            findObject(outputs, im)
+            cv2.imshow(window_name, im)
+            cv2.waitKey(1)
+            key = cv2.waitKey(1)
+            if key == ord('q'):
+                break
+        except http.client.IncompleteRead:
+            print("IncompleteRead caught, skipping frame")
+            continue
+
+    cap.release()
+
+thread1 = threading.Thread(target=fetch_frame, args=(url1, cap1, net, 'Image 1'))
+thread2 = threading.Thread(target=fetch_frame, args=(url2, cap2, net, 'Image 2'))
+
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
+
+cv2.destroyAllWindows()
